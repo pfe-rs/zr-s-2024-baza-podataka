9ab32b5db3fed3ec7e5bf66374280509486bb74e
Nemanja Majski <nemanja.majski05@gmail.com>
2024-03-29 19:33:04 +0100

    Calling the function to read from file

--- tests/test_QueryHandler.py
+++ tests/test_QueryHandler.py
@@ -10,13 +10,13 @@ import os
 
 qh = QueryHandler()
 
-print(qh.parseInput(os.getcwd() + "/tests/query-samples/create.json"))
-print(qh.parseInput(os.getcwd() + "/tests/query-samples/insert.json"))
-print(qh.parseInput(os.getcwd() + "/tests/query-samples/select.json"))
-print(qh.parseInput(os.getcwd() + "/tests/query-samples/delete.json"))
-print(qh.parseInput(os.getcwd() + "/tests/query-samples/insert.json"))
-print(qh.parseInput(os.getcwd() + "/tests/query-samples/update.json"))
-print(qh.parseInput(os.getcwd() + "/tests/query-samples/drop.json"))
+print(qh.readInputFromFile(os.getcwd() + "/tests/query-samples/create.json"))
+print(qh.readInputFromFile(os.getcwd() + "/tests/query-samples/insert.json"))
+print(qh.readInputFromFile(os.getcwd() + "/tests/query-samples/select.json"))
+print(qh.readInputFromFile(os.getcwd() + "/tests/query-samples/delete.json"))
+print(qh.readInputFromFile(os.getcwd() + "/tests/query-samples/insert.json"))
+print(qh.readInputFromFile(os.getcwd() + "/tests/query-samples/update.json"))
+print(qh.readInputFromFile(os.getcwd() + "/tests/query-samples/drop.json"))
 
 
 
